26cfca47f1f2ec749cb5a5586b1f9f1d1ede602e
hamzaoda <97050670+hamzaoda@users.noreply.github.com>
2023-04-10 17:49:59 +0400

    add another url for the api

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,10 +3,17 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
     console.log("are we here")
     app.use(
-        "/",
+        "/api",
         createProxyMiddleware({
             target: "https://hamza-s-shop-production.up.railway.app/",
             changeOrigin: true,
         })
-    );
+    ),
+   app.use(
+        "/user",
+        createProxyMiddleware({
+            target: "https://hamza-s-shop-production.up.railway.app/",
+            changeOrigin: true,
+        })
+    )     
 };
