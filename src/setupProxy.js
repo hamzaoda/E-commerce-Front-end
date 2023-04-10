0a3f046cca085f78e1f46f8fa4fdec53ff8a61cf
hamzaoda <97050670+hamzaoda@users.noreply.github.com>
2023-04-10 17:29:09 +0400

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
     app.use(
-        "/",
+        "/api",
         createProxyMiddleware({
             target: "https://hamza-s-shop-production.up.railway.app/",
             changeOrigin: true,
