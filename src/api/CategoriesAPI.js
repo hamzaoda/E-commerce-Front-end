afb1fa4a1c12343b13187b5a449d0a482aa1dce5
hamzaoda <97050670+hamzaoda@users.noreply.github.com>
2023-04-10 15:09:58 +0400

    Update Cateogries.API

--- src/api/CategoriesAPI.js
+++ src/api/CategoriesAPI.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function CategoriesAPI() {
     const [categories, setCategories] = useState([]);
